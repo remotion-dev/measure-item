515966fa414e41bb976c1f0df274cc738019b9dd
Jonny Burger <jonathanburger11@gmail.com>
2024-01-31 15:43:31 +0100

    upgrade remotion

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,94 +1,49 @@
-const MEASURER_SIZE = 10;
-
-type Result = {
-	getBoundingClientRectHeight: number;
-	getBoundingClientRectWidth: number;
-	correctedHeight: number;
-	correctedWidth: number;
-};
-
 import {AbsoluteFill} from 'remotion';
 import {useCallback, useEffect} from 'react';
 import {useState} from 'react';
 import {useRef} from 'react';
 
+const MEASURER_SIZE = 10;
+
 export const MyComposition = () => {
 	const ref = useRef<HTMLDivElement>(null);
 	const measurer = useRef<HTMLDivElement>(null);
 
-	const [state, setState] = useState<Result | null>(null);
+	const [dimensions, setSimensions] = useState<{
+		correctedHeight: number;
+		correctedWidth: number;
+	} | null>(null);
 
 	const measure = useCallback(() => {
-		const {current} = ref;
-		const measurerCurrent = measurer.current;
-		if (!current || !measurerCurrent) {
+		if (!ref.current || !measurer.current) {
 			return;
 		}
 
-		const rect = current.getBoundingClientRect();
-		const measurerRect = measurerCurrent.getBoundingClientRect();
+		const rect = ref.current.getBoundingClientRect();
+		const measurerRect = measurer.current.getBoundingClientRect();
 		const scale = MEASURER_SIZE / measurerRect.width;
-		const correctedHeight = rect.height * scale;
-		const correctedWidth = rect.width * scale;
 
-		setState({
-			getBoundingClientRectHeight: rect.height,
-			getBoundingClientRectWidth: rect.width,
-			correctedHeight,
-			correctedWidth,
+		setSimensions({
+			correctedHeight: rect.height * scale,
+			correctedWidth: rect.width * scale,
 		});
 	}, []);
 
 	useEffect(() => {
-		const interval = setInterval(measure, 200);
-		return () => {
-			clearInterval(interval);
-		};
+		measure();
 	}, [measure]);
 
 	return (
-		<AbsoluteFill
-			style={{
-				justifyContent: 'center',
-				alignItems: 'center',
-				fontFamily: 'sans-serif',
-			}}
-		>
-			<div
-				ref={ref}
-				style={{
-					border: '3px solid black',
-					fontSize: 100,
-					display: 'inline-block',
-				}}
-			>
-				Hello World!
-			</div>
+		<AbsoluteFill>
+			<div ref={ref}>Hello World!</div>
 			<div
 				ref={measurer}
 				style={{
-					height: MEASURER_SIZE,
 					width: MEASURER_SIZE,
 					position: 'fixed',
 					top: -99999,
 				}}
 			/>
-			{state ? (
-				<div
-					style={{
-						background: 'black',
-						color: 'white',
-						padding: 10,
-						fontSize: 30,
-						textAlign: 'center',
-					}}
-				>
-					getBoundingClientRect(): {state.getBoundingClientRectWidth.toFixed(0)}
-					x{state.getBoundingClientRectHeight.toFixed(0)} <br />
-					Natural size: {state.correctedWidth.toFixed(0)}x
-					{state.correctedHeight.toFixed(0)}
-				</div>
-			) : null}
 		</AbsoluteFill>
 	);
 };
